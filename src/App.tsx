4b298f3db56b3f980944445f60fe1c3e9117b401
Ilan Olkies <ilanolkies@outlook.com>
2020-07-05 00:40:02 -0300

    Create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.sass';
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <Header />
+      </div>
+      <div className="body">
+        <Body />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Body/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.sass'
+
+export default () => (
+  <div className="Body">
+    <div className="sidebar">
+      sidebar
+    </div>
+    <div className="divisor"></div>
+    <div className="canvas">
+      canvas
+    </div>
+  </div>
+)
