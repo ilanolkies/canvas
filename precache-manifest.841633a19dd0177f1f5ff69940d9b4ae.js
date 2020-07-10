48d2f2d9b3f1fdfebaad35e2cc2fe5da90763a61
Ilan Olkies <ilanolkies@outlook.com>
2020-07-10 00:13:20 -0300

    Updates

---
+++ precache-manifest.841633a19dd0177f1f5ff69940d9b4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7f97e932c9537d2ff0ebc3c0c6f926",
+    "url": "/canvas/index.html"
+  },
+  {
+    "revision": "753747bb9fd067b7fe51",
+    "url": "/canvas/static/css/main.1dd5f89c.chunk.css"
+  },
+  {
+    "revision": "fcc090a6b0adfe93d2c9",
+    "url": "/canvas/static/js/2.af6a5260.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/canvas/static/js/2.af6a5260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753747bb9fd067b7fe51",
+    "url": "/canvas/static/js/main.f847a6c6.chunk.js"
+  },
+  {
+    "revision": "be4f1718dc80088842fd",
+    "url": "/canvas/static/js/runtime-main.fe95e63f.js"
+  }
+]);
