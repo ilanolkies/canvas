40f91752e3ce76d3c72b12aa992a8f0558877f6b
Ilan Olkies <ilanolkies@outlook.com>
2020-07-10 22:33:32 -0300

    Add redux logger

--- src/store.ts
+++ src/store.ts
@@ -3,6 +3,14 @@ import reducer from './reducer'
 
 export type RootState = ReturnType<typeof reducer>
 
+const middleware: any[] = [];
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+
+  middleware.push(logger);
+}
+
 export const createStore = () => {
-  return configureStore({ reducer })
+  return configureStore({ reducer, middleware })
 }
